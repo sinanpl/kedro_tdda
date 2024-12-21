58bac9024ec023f9acc27e445d022c04f85fcdd0
Sinan Polatoglu <sinan.polatoglu@outlook.com>
2024-12-21 15:14:04 +0100

    update

--- docs/fix-shell.py
+++ docs/fix-shell.py
@@ -6,7 +6,6 @@ with open(filepath, 'r') as file:
     html_string = "".join(file.readlines())
 
 pattern="""<span class="op">!</span>"""
-pattern="""INFO - Using.+as logging configuration\n"""
 repl_html_string = re.sub(pattern, repl="", string=html_string)
 
 with open(filepath, 'w') as file:
